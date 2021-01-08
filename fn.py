ac0c972605176dce5ddc0798dec2031a4f8c34e6
PabloGrilletti <pgrilletti@gmail.com>
2021-01-07 22:19:07 -0300

    Segundo commit. Puedo agregar un item a la base de datos.

--- fn.py
+++ fn.py
@@ -7,3 +7,24 @@ def run_query(query, parameters = ()):
 		result = cursor.execute(query, parameters)
 		conn.commit()
 	return result
+
+def validateDate(date):
+	i = 0
+	for c in date:
+		if c == '-':
+			i +=1
+	if i == 2:
+		try:
+			year = int(date[0:4])
+			month = int(date[5:7])
+			day = int(date[8:10])
+		except:
+			return False
+
+
+		if year > 1900 and year < 2100:
+			if month > 0 and month < 13:
+				if day > 0 and day < 32:
+					return True
+	
+	return False
--- main.py
+++ main.py
@@ -9,12 +9,14 @@ class Mainapp:
         self.wind = window
         self.wind.title('Control gastos mensuales')
         self.wind.geometry('500x250')
+        self.wind['bg'] = '#328be0'
+        
 
         tk.Button(self.wind, text = 'Tarjetas de Credito', width = 20, height = 5,
-        command = self.CreditCard).grid(row = 1, column = 1)
+        command = self.CreditCard, bg = '#ffb63e').grid(row = 1, column = 1)
     
     def CreditCard(self):
-        self.cCard = Toplevel()
+        self.cCard = Toplevel(window)
         self.cCard.title('Listado de Tarjetas de Credito')
         self.cCard.geometry('550x250')
 
@@ -35,20 +37,88 @@ class Mainapp:
         self.tree.column('#4', width = 100)
         self.tree.heading('#4', text = 'Total')
 
+        self.tree.bind('<<TreeviewSelect>>', self.selectedItem)
+
         frameBtn = tk.LabelFrame(self.cCard)
         frameBtn.place(x = 10, y = 200)
 
-        tk.Button(frameBtn, text = 'Agregar', width = 50).grid(row = 0, column = 0)
-        
-
+        tk.Button(frameBtn, text = 'Agregar', width = 17, command = self.addCard).grid(row = 0, column = 0, padx = 5)
+        tk.Button(frameBtn, text = 'Editar', width = 17, command = self.editCard).grid(row = 0, column = 1, padx = 5)
+        tk.Button(frameBtn, text = 'Cerrar', width = 17, command = self.cCard.destroy).grid(row = 0, column = 2, padx = 5)
+            
         self.get_cCards()
+
+    def addCard(self):
+        self.addCard = Toplevel(self.cCard)
+        self.addCard.title('Agregar Tarjeta de Credito')
+        self.addCard.geometry('500x150')
+
+        frame = tk.LabelFrame(self.addCard)
+        frame.place(x = 10, y = 10)
+
+        tk.Label(frame, text = 'Nombre').grid(row = 0, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.addName = tk.Entry(frame, width = 20)
+        self.addName.grid(row = 0, column = 1, padx = 5, pady = 5)
+        tk.Label(frame, text = 'Fecha Cierre').grid(row = 1, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.addDeadline = tk.Entry(frame, width = 10)
+        self.addDeadline.grid(row = 1, column = 1, padx = 5, pady = 5, sticky = 'w')
+        tk.Label(frame, text = 'Fecha Vencimiento').grid(row = 2, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.addDueDate = tk.Entry(frame, width = 10)
+        self.addDueDate.grid(row = 2, column = 1, padx = 5, pady = 5, sticky = 'w')
+        tk.Label(frame, text = 'Total').grid(row = 3, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.addBalance = tk.Entry(frame, width = 10)
+        self.addBalance.grid(row = 3, column = 1, padx = 5, pady = 5, sticky = 'w')
+
+        tk.Button(self.addCard, text = 'Aceptar', width = 15, height = 3, command = self.add_cCards).place(x = 340, y = 10)
+        tk.Button(self.addCard, text = 'Cancelar', width = 15, height = 3, command = self.addCard.destroy).place(x = 340, y = 80)
+
+    def editCard(self):
+        self.editCard = Toplevel(self.cCard)
+        self.editCard.title('Editar Tarjeta de Credito')
+        self.editCard.geometry('500x150')
+
+        frame = tk.LabelFrame(self.editCard)
+        frame.place(x = 10, y = 10)
+
+        tk.Label(frame, text = 'Nombre').grid(row = 0, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.editName = tk.Entry(frame, width = 20)
+        self.editName.grid(row = 0, column = 1, padx = 5, pady = 5)
+        tk.Label(frame, text = 'Fecha Cierre').grid(row = 1, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.editDeadline = tk.Entry(frame, width = 10)
+        self.editDeadline.grid(row = 1, column = 1, padx = 5, pady = 5, sticky = 'w')
+        tk.Label(frame, text = 'Fecha Vencimiento').grid(row = 2, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.editDueDate = tk.Entry(frame, width = 10)
+        self.editDueDate.grid(row = 2, column = 1, padx = 5, pady = 5, sticky = 'w')
+        tk.Label(frame, text = 'Total').grid(row = 3, column = 0, padx = 5, pady = 5, sticky = 'w')
+        self.editBalance = tk.Entry(frame, width = 10)
+        self.editBalance.grid(row = 3, column = 1, padx = 5, pady = 5, sticky = 'w')
+
+        tk.Button(self.editCard, text = 'Aceptar', width = 15, height = 3, command = self.add_cCards).place(x = 340, y = 10)
+        tk.Button(self.editCard, text = 'Cancelar', width = 15, height = 3, command = self.editCard.destroy).place(x = 340, y = 80)
+
+        selItem = self.selectedItem()
+        
+        self.editName.insert(0, selItem['values'][0])
+        self.editDeadline.insert(0, selItem['values'][1])
+        self.editDueDate.insert(0, selItem['values'][2])
+        self.editBalance.insert(0, selItem['values'][3])
+
+
+    def selectedItem(self, event = None):
+        selItems = self.tree.selection()
+        if selItems:
+            selItem = selItems[0]
+            lsItem = self.tree.item(selItem)
+
+        return lsItem
+
 
     def get_cCards(self):
         records = self.tree.get_children()
         for elements in records:
             self.tree.delete(elements)
         
-        query = 'SELECT * FROM creditcards ORDER BY id ASC'
+        query = 'SELECT * FROM creditcards ORDER BY id DESC'
         db_rows = fn.run_query(query)
         
         
@@ -56,7 +126,25 @@ class Mainapp:
         
             self.tree.insert('', 0, text = row[0], values = row[1:])
 
+    def add_cCards(self):
+        if self.validar():
+            query = 'INSERT INTO creditcards VALUES(NULL, ?, ?, ?, ?)'
+            if fn.validateDate(self.addDeadline.get()):
+                if fn.validateDate(self.addDueDate.get()):
+                    parameters = (self.addName.get(), self.addDeadline.get(), self.addDueDate.get(), self.addBalance.get())
+                    fn.run_query(query, parameters)
+                    messagebox.showinfo(title = 'Exito', message = 'Tarjeta agregada con éxito', parent = self.addCard)
+                    self.addCard.destroy()
+                    self.get_cCards()
+                else:
+                    messagebox.showerror(title = 'Error', message = 'Formato de fecha no admitido. Solo admite aaaa-mm-dd', parent = self.addCard)
+            else:
+                messagebox.showerror(title = 'Error', message = 'Formato de fecha no admitido. Solo admite aaaa-mm-dd', parent = self.addCard)
+                
 
+
+    def validar(self):
+        return len(self.addName.get()) !=0 and len(self.addDeadline.get()) !=0 and len(self.addDueDate.get()) !=0
 
 if __name__ == "__main__":
     window = tk.Tk()
